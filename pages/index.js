fd59ac946abbc227dbe5ae8f888555275d46f44c
Leonardo S. de A. <149690786+leosimao88br@users.noreply.github.com>
2025-07-31 18:15:25 +0000

    Corrigindo index.

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1>Gabriel Simão Oliveira 06/03/2019 :)</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("teste2");
-
 export default Home;
